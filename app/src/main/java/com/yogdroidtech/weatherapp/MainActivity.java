04a2dfb9dc8b7b9d643a0f6bc1480bf0912e7320
YOGESH <yogrival911@gmail.com>
2020-12-16 15:02:24 +0530

    Weather App Test

--- app/src/main/java/com/yogdroidtech/weatherapp/MainActivity.java
+++ app/src/main/java/com/yogdroidtech/weatherapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.yogdroidtech.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new CurrentFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.currentWeather:
+                        loadFragment(new CurrentFragment());
+                        break;
+                    case R.id.forecast:
+                        loadFragment(new ForecastFragment());
+                        break;
+                    case R.id.city:
+                        loadFragment(new CityFragment());
+                        break;
+                    case R.id.settings:
+                        loadFragment(new SettingsFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
